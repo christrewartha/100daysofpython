91cbfdafadd3540378614b81ac160e926e775d45
Chris Trewartha <christrewartha@hotmail.com>
2023-01-30 13:39:40 +0000

    Create 06_escaping_the_maze.py

---
+++ 06_escaping_the_maze.py
@@ -0,0 +1,32 @@
+example_solution = '''
+# Example Solution
+
+def turn_right():
+    for _ in range(3):
+        turn_left()
+        
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
+            move()
+'''
+
+
+def execute():
+    print("This module uses an online code playground")
+    print("Go to: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json"
+          "&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json")
+    print(example_solution
+          )
+
+if __name__ == '__main__':
+    execute()
